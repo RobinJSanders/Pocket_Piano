76809b3db85236908084beca8ee7cdda6ecba236
RobinJSanders <robinjsanders@yahoo.co.uk>
2017-11-29 18:19:51 +0000

    Change activity_main.xml, MainActivity, colors.xml and styles.xml, Add raw folder with mp3 files for sounds
    All colors used now in colors. xml
    Style.xml uses only colors in colors.xml
    Linear layout added to voice select buttons
    Sounds Added to piano keys
    Voices can now be selected with buttons
    Temporarily removed textviews by commenting them out

--- app/src/main/java/com/rob/pocket_piano/MainActivity.java
+++ app/src/main/java/com/rob/pocket_piano/MainActivity.java
@@ -1,34 +1,31 @@
 package com.rob.pocket_piano;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-
+import static android.R.color.black;
 
 
 public class MainActivity extends AppCompatActivity
 {
     //Piano Keys
-    Button Btn_Low_C;
-    Button Btn_D ;
-    Button Btn_Ds;
-    Button Btn_E;
-    Button Btn_F ;
-    Button Btn_Fs ;
-    Button Btn_G ;
-    Button Btn_Gs;
-    Button Btn_A ;
-    Button Btn_As;
-    Button Btn_B;
-    Button Btn_High_C;
+    Button mBtn_Low_C, mBtn_Cs, mBtn_D, mBtn_Ds, mBtn_E, mBtn_F ,mBtn_Fs, mBtn_G , mBtn_Gs, mBtn_A, mBtn_As, mBtn_B, mBtn_High_C,
     //Voice selection buttons
-    Button Btn_Piano;
-    Button Btn_Bass;
-    Button Btn_Brass;
-    Button Btn_Banjo;
-    Button Btn_Synth;
+    mBtn_Piano, mBtn_Bass, mBtn_Brass, mBtn_Banjo, mBtn_Synth;
+    //sound IDs
+    private int mSound_Low_C, mSound_Cs ,mSound_D, mSound_Ds, mSound_E, mSound_F ,mSound_Fs, mSound_G , mSound_Gs, mSound_A, mSound_As, mSound_B, mSound_High_C,
+    // Colors
+    mColor_Black, mColor_White, mColor_LightBlue;
+
+    SoundPool mSoundPool;
+
+
 
 
     @Override
@@ -37,173 +34,216 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Initialise piano keys
-        Btn_Low_C =(Button) findViewById(R.id.Btn_Low_C);
-        Btn_D =(Button) findViewById(R.id.Btn_D);
-        Btn_Ds =(Button) findViewById(R.id.Btn_Ds);
-        Btn_E =(Button) findViewById(R.id.Btn_E);
-        Btn_F =(Button) findViewById(R.id.Btn_F);
-        Btn_Fs =(Button) findViewById(R.id.Btn_Fs);
-        Btn_G =(Button) findViewById(R.id.Btn_Gs);
-        Btn_Gs =(Button) findViewById(R.id.Btn_Gs);
-        Btn_A =(Button) findViewById(R.id.Btn_A);
-        Btn_As =(Button) findViewById(R.id.Btn_As);
-        Btn_B =(Button) findViewById(R.id.Btn_B);
-        Btn_High_C =(Button) findViewById(R.id.Btn_High_C);
 
-        //Initialise Voice Buttons
-        Btn_Piano = (Button) findViewById(R.id.Btn_Piano);
-        Btn_Bass = (Button) findViewById(R.id.Btn_Bass);
-        Btn_Brass = (Button) findViewById(R.id.Btn_Brass);
-        Btn_Banjo = (Button) findViewById(R.id.Btn_Banjo);
-        Btn_Synth = (Button) findViewById(R.id.Btn_Synth);
+
+        //assign references to views for keyboard keys
+        mBtn_Low_C =(Button) findViewById(R.id.Btn_Low_C);
+        mBtn_Cs = (Button) findViewById(R.id.Btn_Cs);
+        mBtn_D =(Button) findViewById(R.id.Btn_D);
+        mBtn_Ds =(Button) findViewById(R.id.Btn_Ds);
+        mBtn_E =(Button) findViewById(R.id.Btn_E);
+        mBtn_F =(Button) findViewById(R.id.Btn_F);
+        mBtn_Fs =(Button) findViewById(R.id.Btn_Fs);
+        mBtn_G =(Button) findViewById(R.id.Btn_G);
+        mBtn_Gs =(Button) findViewById(R.id.Btn_Gs);
+        mBtn_A =(Button) findViewById(R.id.Btn_A);
+        mBtn_As =(Button) findViewById(R.id.Btn_As);
+        mBtn_B =(Button) findViewById(R.id.Btn_B);
+        mBtn_High_C =(Button) findViewById(R.id.Btn_High_C);
+
+
+        mBtn_Piano = (Button) findViewById(R.id.Btn_Piano);
+        mBtn_Bass = (Button) findViewById(R.id.Btn_Bass);
+        mBtn_Brass = (Button) findViewById(R.id.Btn_Brass);
+        mBtn_Banjo = (Button) findViewById(R.id.Btn_Banjo);
+        mBtn_Synth = (Button) findViewById(R.id.Btn_Synth);
+
+
+        //Initialise SoundPool
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP)
+        {
+            mSoundPool = new SoundPool.Builder().setMaxStreams(5).build();
+        }
+        else
+        {
+            mSoundPool = new SoundPool(5, AudioManager.STREAM_MUSIC,0);
+        }
+
+        //Get colors from resources folder
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M)
+        {
+            mColor_Black = ContextCompat.getColor(this,R.color.black);
+            mColor_White = ContextCompat.getColor(this,R.color.white);
+            mColor_LightBlue = ContextCompat.getColor(this, R.color.lightBlue);
+        }
+        else
+        {
+            mColor_Black = getResources().getColor(R.color.black);
+            mColor_White = getResources().getColor(R.color.white);
+            mColor_LightBlue = getResources().getColor(R.color.lightBlue);
+        }
+
+
+        //load initial sounds for piano
+        loadPianoSounds();
 
         //OnClickListeners for each piano key
-        Btn_Low_C.setOnClickListener(new View.OnClickListener()
+        mBtn_Low_C.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                mSoundPool.play(mSound_Low_C,1,1,1,0,1);
             }
         });
 
-        Btn_D.setOnClickListener(new View.OnClickListener()
+        mBtn_Cs.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                mSoundPool.play(mSound_Cs,1,1,1,0,1);
             }
         });
 
-        Btn_Ds.setOnClickListener(new View.OnClickListener()
+
+        mBtn_D.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                mSoundPool.play(mSound_D,1,1,1,0,1);
             }
         });
 
-        Btn_E.setOnClickListener(new View.OnClickListener()
+        mBtn_Ds.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
-            }
-        });
-        Btn_F.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-
+                mSoundPool.play(mSound_Ds,1,1,1,0,1);
             }
         });
 
-        Btn_Fs.setOnClickListener(new View.OnClickListener()
+        mBtn_E.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                mSoundPool.play(mSound_E,1,1,1,0,1);
+            }
+        });
+        mBtn_F.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mSoundPool.play(mSound_F,1,1,1,0,1);
             }
         });
 
-        Btn_G.setOnClickListener(new View.OnClickListener()
+        mBtn_Fs.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
-            }
-        });
-        Btn_Gs.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-
+                mSoundPool.play(mSound_Fs,1,1,1,0,1);
             }
         });
 
-        Btn_A.setOnClickListener(new View.OnClickListener()
+        mBtn_G.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                mSoundPool.play(mSound_G,1,1,1,0,1);
+            }
+        });
+        mBtn_Gs.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mSoundPool.play(mSound_Gs,1,1,1,0,1);
             }
         });
 
-        Btn_As.setOnClickListener(new View.OnClickListener()
+        mBtn_A.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                mSoundPool.play(mSound_A,1,1,1,0,1);
             }
         });
 
-        Btn_B.setOnClickListener(new View.OnClickListener()
+        mBtn_As.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                mSoundPool.play(mSound_As,1,1,1,0,1);
             }
         });
 
-        Btn_High_C.setOnClickListener(new View.OnClickListener()
+        mBtn_B.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
+                mSoundPool.play(mSound_B,1,1,1,0,1);
+            }
+        });
 
+        mBtn_High_C.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mSoundPool.play(mSound_High_C,1,1,1,0,1);
             }
         });
 
         //OnClickListeners for each voice selector button
-        Btn_Piano.setOnClickListener(new View.OnClickListener()
+        mBtn_Piano.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                loadPianoSounds();
             }
         });
-        Btn_Bass.setOnClickListener(new View.OnClickListener()
+        mBtn_Bass.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                loadBassSounds();
+                mBtn_Bass.setBackgroundColor(getResources().getColor(black));
             }
         });
-        Btn_Brass.setOnClickListener(new View.OnClickListener()
+        mBtn_Brass.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                loadBrassSounds();
             }
         });
-        Btn_Banjo.setOnClickListener(new View.OnClickListener()
+        mBtn_Banjo.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                loadBanjoSounds();
             }
         });
-        Btn_Synth.setOnClickListener(new View.OnClickListener()
+        mBtn_Synth.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-
+                loadSynthSounds();
             }
         });
 
@@ -211,6 +251,92 @@ public class MainActivity extends AppCompatActivity
 
 
     }
+
+    private void loadPianoSounds()
+    {
+        mSound_Low_C = mSoundPool.load(this, R.raw.piano_c,1);
+        mSound_Cs = mSoundPool.load(this, R.raw.piano_cs,1);
+        mSound_D = mSoundPool.load(this, R.raw.piano_d,1);
+        mSound_Ds = mSoundPool.load(this, R.raw.piano_ds,1);
+        mSound_E = mSoundPool.load(this, R.raw.piano_e,1);
+        mSound_F = mSoundPool.load(this, R.raw.piano_f,1);
+        mSound_Fs = mSoundPool.load(this, R.raw.piano_fs,1);
+        mSound_G = mSoundPool.load(this, R.raw.piano_g,1);
+        mSound_Gs = mSoundPool.load(this, R.raw.piano_gs,1);
+        mSound_A = mSoundPool.load(this, R.raw.piano_a,1);
+        mSound_As = mSoundPool.load(this, R.raw.piano_as,1);
+        mSound_B = mSoundPool.load(this, R.raw.piano_b,1);
+        mSound_High_C = mSoundPool.load(this, R.raw.piano_c2,1);
+    }
+
+    private void loadBassSounds()
+    {
+        mSound_Low_C = mSoundPool.load(this, R.raw.bass_c,1);
+        mSound_Cs = mSoundPool.load(this, R.raw.bass_cs,1);
+        mSound_D = mSoundPool.load(this, R.raw.bass_d,1);
+        mSound_Ds = mSoundPool.load(this, R.raw.bass_ds,1);
+        mSound_E = mSoundPool.load(this, R.raw.bass_e,1);
+        mSound_F = mSoundPool.load(this, R.raw.bass_f,1);
+        mSound_Fs = mSoundPool.load(this, R.raw.bass_fs,1);
+        mSound_G = mSoundPool.load(this, R.raw.bass_g,1);
+        mSound_Gs = mSoundPool.load(this, R.raw.bass_gs,1);
+        mSound_A = mSoundPool.load(this, R.raw.bass_a,1);
+        mSound_As = mSoundPool.load(this, R.raw.bass_as,1);
+        mSound_B = mSoundPool.load(this, R.raw.bass_b,1);
+        mSound_High_C = mSoundPool.load(this, R.raw.bass_c2,1);
+    }
+
+    private void loadBrassSounds()
+    {
+        mSound_Low_C = mSoundPool.load(this, R.raw.brass_c,1);
+        mSound_Cs = mSoundPool.load(this, R.raw.brass_cs,1);
+        mSound_D = mSoundPool.load(this, R.raw.brass_d,1);
+        mSound_Ds = mSoundPool.load(this, R.raw.brass_ds,1);
+        mSound_E = mSoundPool.load(this, R.raw.brass_e,1);
+        mSound_F = mSoundPool.load(this, R.raw.brass_f,1);
+        mSound_Fs = mSoundPool.load(this, R.raw.brass_fs,1);
+        mSound_G = mSoundPool.load(this, R.raw.brass_g,1);
+        mSound_Gs = mSoundPool.load(this, R.raw.brass_gs,1);
+        mSound_A = mSoundPool.load(this, R.raw.brass_a,1);
+        mSound_As = mSoundPool.load(this, R.raw.brass_as,1);
+        mSound_B = mSoundPool.load(this, R.raw.brass_b,1);
+        mSound_High_C = mSoundPool.load(this, R.raw.brass_c2,1);
+    }
+
+    private void loadBanjoSounds()
+    {
+        mSound_Low_C = mSoundPool.load(this, R.raw.banjo_c,1);
+        mSound_Cs = mSoundPool.load(this, R.raw.banjo_cs,1);
+        mSound_D = mSoundPool.load(this, R.raw.banjo_d,1);
+        mSound_Ds = mSoundPool.load(this, R.raw.banjo_ds,1);
+        mSound_E = mSoundPool.load(this, R.raw.banjo_e,1);
+        mSound_F = mSoundPool.load(this, R.raw.banjo_f,1);
+        mSound_Fs = mSoundPool.load(this, R.raw.banjo_fs,1);
+        mSound_G = mSoundPool.load(this, R.raw.banjo_g,1);
+        mSound_Gs = mSoundPool.load(this, R.raw.banjo_gs,1);
+        mSound_A = mSoundPool.load(this, R.raw.banjo_a,1);
+        mSound_As = mSoundPool.load(this, R.raw.banjo_as,1);
+        mSound_B = mSoundPool.load(this, R.raw.banjo_b,1);
+        mSound_High_C = mSoundPool.load(this, R.raw.banjo_c2,1);
+    }
+
+    private void loadSynthSounds()
+    {
+        mSound_Low_C = mSoundPool.load(this, R.raw.synth_c,1);
+        mSound_Cs = mSoundPool.load(this, R.raw.synth_cs,1);
+        mSound_D = mSoundPool.load(this, R.raw.synth_d,1);
+        mSound_Ds = mSoundPool.load(this, R.raw.synth_ds,1);
+        mSound_E = mSoundPool.load(this, R.raw.synth_e,1);
+        mSound_F = mSoundPool.load(this, R.raw.synth_f,1);
+        mSound_Fs = mSoundPool.load(this, R.raw.synth_fs,1);
+        mSound_G = mSoundPool.load(this, R.raw.synth_g,1);
+        mSound_Gs = mSoundPool.load(this, R.raw.synth_gs,1);
+        mSound_A = mSoundPool.load(this, R.raw.synth_a,1);
+        mSound_As = mSoundPool.load(this, R.raw.synth_as,1);
+        mSound_B = mSoundPool.load(this, R.raw.synth_b,1);
+        mSound_High_C = mSoundPool.load(this, R.raw.synth_c2,1);
+    }
+
 
 
 }
